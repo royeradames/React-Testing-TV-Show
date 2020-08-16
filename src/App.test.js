fd8f01d01df682ed4533245731e2aa27041d48a5
royeradames <royeraadames@gmail.com>
2020-08-16 14:51:05 -0400

    inital file structure

---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import App from './App'
+import { render, fireEvent, waitFor } from '@testing-library/react'
+
+test('App fetches data and renders it', async () => {
+    //mock data
+
+    //select drop down on component
+
+    //click select
+
+    //component waits for API, then renders data that is returned
+    //use the waitfor function to wait for the API call to resolve
+
+})
---
+++ src/components/Episodes.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react'
+import Episodes from './Episodes'
+
+test('re-renders correctly', () => {
+    //Arrange
+
+    //check that it renderd
+
+    //act
+
+    //Assert
+    
+})
